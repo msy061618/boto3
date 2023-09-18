b8e85436e988ef16378b672096d8a5bfd2936f68
mohan kumar <mohankumar130@gmail.com>
2023-09-18 20:15:01 +0530

    new lines are added in this file

--- Volume_modify.py
+++ Volume_modify.py
@@ -7,13 +7,16 @@ volume_describe = aws_client.describe_volumes()
 
 for volume in volume_describe["Volumes"]:
     all_volume_id = volume["VolumeId"]
-    if volume["VolumeType"] == "gp2":
-        volume_id = volume["VolumeId"]
-        print(volume_id)
-        response = aws_client.modify_volume(
-            VolumeId = volume_id,
-            VolumeType = "gp3"
-        )
-        print(f"Volume {volume_id} has modified to gp3")
+    if volume["Size"] == "50":
+        if volume["VolumeType"] == "gp2":
+            volume_id = volume["VolumeId"]
+            print(volume_id)
+            response = aws_client.modify_volume(
+                VolumeId = volume_id,
+                VolumeType = "gp3"
+            )
+            print(f"Volume {volume_id} has modified to gp3")
+        else:
+            print(f"Already volume {all_volume_id} is gp3 only")
     else:
-        print(f"Already volume {all_volume_id} is gp3 only")
+        print(f"This Volume id {all_volume_id} is more than 50 GB")
