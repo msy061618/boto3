883a8cedda8281e1d33b349bcc5bfef066b00f97
msy <mohankumar130@gmail.com>
2023-10-12 12:25:32 +0530

    new lines added

--- elb.py
+++ elb.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 elb_list = []
 target_list = []
+classic_elb_list = []
 aws_session = boto3.Session(profile_name="default", region_name="us-east-1")
-aws_client = aws_session.client(service_name="elbv2")
-response = aws_client.describe_load_balancers()
+aws_client_elbv2 = aws_session.client(service_name="elbv2")
+aws_client_elb = aws_session.client(service_name="elb")
+
+response = aws_client_elbv2.describe_load_balancers()
 for elb in response["LoadBalancers"]:
     alb_details = {
     "Loadbalancer_name" : elb["LoadBalancerName"],
@@ -14,18 +17,19 @@ for elb in response["LoadBalancers"]:
     "ELB_Scheme" : elb["Scheme"],
     "ARN" : elb["LoadBalancerArn"]
     }
-    print(alb_details)
+    # print(alb_details)
 
     elb_list.append(alb_details)
 
-response1 = aws_client.describe_target_groups()
+response1 = aws_client_elbv2.describe_target_groups()
 for target in response1["TargetGroups"]:
     arn = target["TargetGroupArn"]
-    demo = aws_client.describe_target_health(TargetGroupArn = arn)
+    print(arn)
+    demo = aws_client_elbv2.describe_target_health(TargetGroupArn = arn)
     targetin = demo["TargetHealthDescriptions"]
     for j in targetin:
         Instance_ID = j["Target"]["Id"]
-        print(Instance_ID)
+        # print(Instance_ID)
         target_dtatils = {
             "TG_Name" : target["TargetGroupName"],
             "TG_Port" : target["Port"],
@@ -35,12 +39,28 @@ for target in response1["TargetGroups"]:
         print(target_dtatils)
         target_list.append(target_dtatils)
 
+classic_response = aws_client_elb.describe_load_balancers()
+
+for i in classic_response["LoadBalancerDescriptions"]:
+    for j in i["Instances"]:
+        classic_elb = {
+        "Instance_Id" : j["InstanceId"],
+        "LoadBalancerName" : i["LoadBalancerName"],
+        "DNSName" : i["DNSName"],
+        "LoadBalancerPort" : i["ListenerDescriptions"][0]["Listener"]["LoadBalancerPort"],
+        "InstancePort" : i["ListenerDescriptions"][0]["Listener"]["InstancePort"]
+        }
+        print(classic_elb)
+        classic_elb_list.append(classic_elb)
+
 df = pd.DataFrame(elb_list)
 df1 = pd.DataFrame(target_list)
+df2 = pd.DataFrame(classic_elb_list)
 
 excel_writer = pd.ExcelWriter('albv2.xlsx', engine='xlsxwriter')
 
-df.to_excel(excel_writer,sheet_name="Sheet1", index=False)
-df1.to_excel(excel_writer,sheet_name="Sheet2", index=False)
+df.to_excel(excel_writer,sheet_name="alb_details", index=False)
+df1.to_excel(excel_writer,sheet_name="tg_details", index=False)
+df2.to_excel(excel_writer,sheet_name="clb_details", index=False)
 
 excel_writer.close()
